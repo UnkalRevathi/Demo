9172474cdf02fc63d5401c8c1941d3141e797567
UnkalRevathi <unkalrevathi@gmail.com>
2025-04-24 11:09:44 +0530

    saved

---
+++ Demo.java
@@ -0,0 +1,11 @@
+public class Demo {
+     public static void main(String[]args){
+        int a=10;
+        int b=15;
+        int x,y;
+        x=a+b;
+        y=a-b;
+        System.out.println("Addition is: "+x);
+        System.out.println("subtraction is: "+y);
+     }
+}
